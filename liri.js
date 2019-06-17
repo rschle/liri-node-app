82962345a3291a4332e72ef4c0809001bf33edc4
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-06-16 18:08:19 -0700

    started working on the cconcert one

--- liri.js
+++ liri.js
@@ -1,7 +1,35 @@
+//requiring the .env file
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var request = require("request");
 var fs = require("fs");
-
+var axios = require("axios");
+var moment = require("moment")
+//spotify 
+var spotify = require("node-spotify-api)
 var spotify = new Spotify(keys.spotify);
 
+
+//defining elements of our array (command and query request)
+var command = process.argv[2];
+var query = process.argv[3];
+
+//need to make the commands
+if(command === "concert-this") {
+    var artist = process.argv[3];
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+axios
+    .get(queryURL);
+    .then(function(response){
+        
+
+        console.log("Movie release year: " + response.data.Year);
+    })
+} else if(command === "spotify-this"){
+
+}else if(command === "movie-this"){
+
+}else if(command === "do-what-it-says"){
+
+}
