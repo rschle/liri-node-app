16df4aecde4499a7050242ff8c03eb512850bc1b
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-06-17 14:38:46 -0700

    more progress on concert part

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment")
 //spotify 
-var spotify = require("node-spotify-api)
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 
@@ -17,19 +17,33 @@ var query = process.argv[3];
 
 //need to make the commands
 if(command === "concert-this") {
-    var artist = process.argv[3];
+    var artist = process.argv.slice(3).join("");
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 axios
-    .get(queryURL);
+    .get(queryURL)
     .then(function(response){
+    for(var i = 0; i < response.data.length; i++){
+        //venue name
+        console.log("Venue Name: " + response.data[i].venue.name);
+        //location
+        if(response.data[i].venue.region){
+            console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region)
+        }
+        else{
+            console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country)
+        }
+        console.log("Event Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+    }
+        // console.log(response.data);
         
-
-        console.log("Movie release year: " + response.data.Year);
+        // console.log(response.data.venue.city);
+        // console.log(response.data.venue.state);
+        // console.log(moment(response.datetime).format("MM/DD/YYYY"));
     })
-} else if(command === "spotify-this"){
+// }else if(command === "spotify-this"){
 
-}else if(command === "movie-this"){
+// }else if(command === "movie-this"){
 
-}else if(command === "do-what-it-says"){
+// }else if(command === "do-what-it-says"){
 
-}
+ }
